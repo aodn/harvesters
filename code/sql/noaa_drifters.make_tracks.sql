a1f12f5b443a7ba73aac35e8ba339f47008c540c
mhidas <marty.hidas@utas.edu.au>
2014-03-19 17:06:34 +1100

    break up tracks into day sections

--- code/sql/noaa_drifters.make_tracks.sql
+++ code/sql/noaa_drifters.make_tracks.sql
@@ -1,12 +1,21 @@
 "
-INSERT INTO track(driftnum, file_id, geom, time_coverage_start, time_coverage_end)
+INSERT INTO track(driftnum, file_id, geom, date_utc, duration)
   SELECT 
-    driftnum,
-    file_id,
-    make_trajectory(geom ORDER BY date),
-    min(date), 
-    max(date)
-  FROM measurement
-  WHERE file_id = '" + context.fileId + "'
-  GROUP BY driftnum, file_id
+    driftnum, 
+    file_id, 
+    make_trajectory(geom ORDER BY time),
+    date_utc,
+    max(time) - min(time)
+  FROM (
+    SELECT driftnum,  file_id, geom, date AS time,
+      date(date AT TIME ZONE 'UTC') AS date_utc
+      FROM measurement
+    UNION
+    SELECT driftnum,  file_id, geom, date AS time,
+      date(date AT TIME ZONE 'UTC' - interval '6 hours') AS date_utc
+      FROM measurement
+    ) AS dates
+  WHERE file_id = " + context.fileId + "
+  GROUP BY driftnum, file_id, date_utc
+  ORDER BY driftnum, file_id, date_utc
 ;"
