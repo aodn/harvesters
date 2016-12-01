c4aff94edd78c3895bab834dafeec37e442eaf3b
mhidas <marty.hidas@utas.edu.au>
2016-12-01 11:16:30 +1100

    AODN_NSW_OEH: update file-naming convention

--- workspace/AODN_NSW_OEH/code/sql/get_survey_date_location.sql
+++ workspace/AODN_NSW_OEH/code/sql/get_survey_date_location.sql
@@ -1,5 +1,5 @@
 "SELECT
-  substring(url, '/[[:digit:]]+_([[:alnum:]]+)_[^/]+$') AS location,
-  substring(url, '/([[:digit:]]+)_[^/]+$')::date AS survey_date
+  substring(url, '/NSWOEH_[[:digit:]]+_([[:alnum:]]+)_[^/]+$') AS location,
+  substring(url, '/NSWOEH_([[:digit:]]+)_[^/]+$')::date AS survey_date
 FROM indexed_file
 WHERE id = " + context.file_id + ";"
