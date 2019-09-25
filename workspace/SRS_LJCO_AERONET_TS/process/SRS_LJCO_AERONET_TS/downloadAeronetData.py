975cb6c0a1a93d08a0569243df21ac0b4d7cf2fb
lbesnard <laurent.besnard@utas.edu.au>
2019-09-25 16:05:41 +1000

    Revert "Update Python using python-modernize -n -w -p on this repo"

--- workspace/SRS_LJCO_AERONET_TS/process/SRS_LJCO_AERONET_TS/downloadAeronetData.py
+++ workspace/SRS_LJCO_AERONET_TS/process/SRS_LJCO_AERONET_TS/downloadAeronetData.py
@@ -6,31 +6,32 @@ folder aeronetUnZipFolder preferably such as /tmp/aeronet
 @author: lbesnard
 
 """
-from BeautifulSoup import BeautifulSoup
-import six.moves.urllib.request, six.moves.urllib.error, six.moves.urllib.parse, six.moves.urllib.request, six.moves.urllib.parse, six.moves.urllib.error
-import re
-import zipfile
 
 def download_aeronetData (aeronetUnZipFolder):
+    from BeautifulSoup import BeautifulSoup
+    import urllib2,urllib
+    import re
+    import zipfile
     
     # we remove all *.lev20 files previously downloaded in aeronetUnZipFolder
-    purge(aeronetUnZipFolder, '.*lev20$')
+    purge (aeronetUnZipFolder,'.*lev20$')
     
     # open NASA page where data url can be found 
-    html_page = six.moves.urllib.request.urlopen("http://aeronet.gsfc.nasa.gov/cgi-bin/print_warning_opera_v2_new?site=Lucinda&year=110&month=6&day=1&year2=110&month2=6&day2=30&LEV20=1&AVG=10")
+    html_page = urllib2.urlopen("http://aeronet.gsfc.nasa.gov/cgi-bin/print_warning_opera_v2_new?site=Lucinda&year=110&month=6&day=1&year2=110&month2=6&day2=30&LEV20=1&AVG=10")
     soup = BeautifulSoup(html_page)
        
     # scrap webpage to find zip file address
     for link in  soup.findAll('a', attrs={'href': re.compile("^.zip")}):
         web_adress = 'http://aeronet.gsfc.nasa.gov' + link.get('href')
-
+    
+    
     aeronetZip = "/tmp/aeronet.zip"   
-    downloadedFile = six.moves.urllib.request.URLopener()
-    print("Download data file to " + aeronetZip)
+    downloadedFile = urllib.URLopener()
+    print 'Download data file to '+ aeronetZip
     downloadedFile.retrieve(web_adress, aeronetZip)
          
     zip = zipfile.ZipFile(aeronetZip)  
-    print("Extract file to " + aeronetUnZipFolder)
+    print 'Extract file to ' + aeronetUnZipFolder
     zip.extractall(aeronetUnZipFolder)  
     return 1
     
@@ -40,19 +41,19 @@ def purge(dir, pattern):
     import os, re
 
     for f in os.listdir(dir):
-        if re.search(pattern, f):
-            os.remove(os.path.join(dir, f))
+    	if re.search(pattern, f):
+    		os.remove(os.path.join(dir, f))
     return 1 
     
 if __name__ == "__main__":
     try:
         import sys,os
-        print("Folder Output: " + str(sys.argv[1]))
+        print 'Folder Output:', str(sys.argv[1])
         aeronetUnZipFolder = str(sys.argv[1])
         
         if not os.path.exists(aeronetUnZipFolder):
             os.makedirs(aeronetUnZipFolder)
         
-        download_aeronetData(aeronetUnZipFolder)
-    except Exception as e:
-        print(e)
+        download_aeronetData (aeronetUnZipFolder)
+    except Exception, e:
+        print 
